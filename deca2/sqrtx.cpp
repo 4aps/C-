09ca4771e9df8b3dc6de7644fc57681cb221ec51
Akhand Pratap <132538768+4aps@users.noreply.github.com>
2025-07-18 18:37:57 +0530

    Update sqrtx.cpp
    69. Sqrt(x) {LeetCode}
    
    Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
    
    You must not use any built-in exponent function or operator.
    
    For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
    
    Example 1:
    
    Input: x = 4
    Output: 2
    Explanation: The square root of 4 is 2, so we return 2.
    Example 2:
    
    Input: x = 8
    Output: 2
    Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
     
    
    Constraints:
    0 <= x <= 231 - 1

--- deca2/sqrtx.cpp
+++ deca2/sqrtx.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 int returnRoot(int squaredNum){
-
     if(squaredNum < 2)
     return squaredNum;
     int start = 0, end = squaredNum, ans;
@@ -22,10 +21,8 @@ int returnRoot(int squaredNum){
 
 int main(){
     int targetNum;
-
     cout << "Enter target value: ";
     cin >> targetNum;
-
     cout << returnRoot(targetNum);
     return 0; 
 }
