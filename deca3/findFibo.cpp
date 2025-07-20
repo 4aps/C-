9839f4cd0c87ae6b023faa4c6af9e62af0cfab20
Akhand Pratap <132538768+4aps@users.noreply.github.com>
2025-07-20 23:52:06 +0530

    Update findFibo.cpp

--- deca3/findFibo.cpp
+++ deca3/findFibo.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int n, arr[100];
-    cout << "Enter the size: ";
+int main() {
+    int n;
     cin >> n;
-
-    arr[0] = 0;
-    arr[1] = 1;
-
-    for(int i = 2; i < n; i++){
-        arr[i] = arr[i-1] + arr[i-2];
+    long long a = 0, b = 1, c;
+    for (int i = 0; i < n; i++) {
+        cout << a << " ";
+        c = a + b;
+        a = b;
+        b = c;
     }
-
-    cout << arr[n-1];
     return 0;
 }
